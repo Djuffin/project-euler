8c038af95698e5132845b0a5d066301525f6fb61
Eugene Zemtsov <e.zemtsov@mirantis.ru>
2010-05-27 03:42:24 +0400

    problem 68 solved

---
+++ Haskell/src/Problem68.hs
@@ -0,0 +1,35 @@
+module Problem68 where
+import System.IO
+import Numeric
+
+
+getPairs :: [Int] -> [(Int, Int)]
+getPairs [] = []
+getPairs (a:[]) = []
+getPairs (a:b:xs) = (a, b) : (getPairs (b:xs))
+
+getMax ((a, b), c) = max (a + c) (b + c)
+
+
+getNextLine :: [Int] -> [Int] -> [Int]
+getNextLine line costs = map getMax ways
+            where
+                ways = zip pairs costs
+                pairs = getPairs line'
+                line' = [0] ++ line ++ [0]
+
+getLastLine lines = foldl getNextLine [] lines
+
+
+answer input =  maximum $ getLastLine input
+
+main :: IO ()
+main = do
+        inh <- openFile "input68.txt" ReadMode
+        content <- hGetContents inh
+        let contentLines = lines content
+        let input = map (map read . words) contentLines :: [[Int]]
+        putStrLn $ show $ answer input
+
+
+

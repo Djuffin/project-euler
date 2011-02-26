1ccaed79f02bbaaa99ebd7bdce4f8231ad6afdd7
Eugene Zemtsov <e.zemtsov@gmail.com>
2011-02-27 00:01:33 +0300

    smaller solution for 48

--- Haskell/src/Problem48.hs
+++ Haskell/src/Problem48.hs
@@ -1,14 +1,6 @@
 module Main where 
 
-pow :: Integer -> Integer -> Integer
-pow x y = pow2 x y 1
-            where
-              pow2 x 0 acc = acc
-              pow2 x y acc = pow2 x (y - 1) (acc * x)
-
 f :: Integer -> Integer
-f x = pow x x
-
-
+f x = x^x
 
 main = do putStrLn $ show $ (sum $ map f [1..1000]) `mod` (f 10)
